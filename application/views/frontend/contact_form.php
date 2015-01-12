d7014fc3bc1f31411007fc07e6d6fcbe3640899d
Ahmed Samir <ahmed.samir@basharsoft.com>
2015-01-13 00:02:53 +0200

    commiting

--- application/views/frontend/contact_form.php
+++ application/views/frontend/contact_form.php
@@ -37,7 +37,7 @@
 </style>
 
 <div class="inside-banner">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6908.2709880403245!2d31.247230605005115!3d30.032970436627593!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xd9f38fe9e0c087c1!2z2KfZhNix2LTZitiv2Yog2KfZhNmF2YrYstin2YY!5e0!3m2!1sar!2seg!4v1421099689495" width="100%" height="367" frameborder="0" style="border:0"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6908.2709880403245!2d31.247230605005115!3d30.032970436627593!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xd9f38fe9e0c087c1!2z2KfZhNix2LTZitiv2Yog2KfZhNmF2YrYstin2YY!5e0!3m2!1sar!2seg!4v1421099689495" width="100%" height="347" frameborder="0" style="border:0"></iframe>
     <!--<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2428.619994812634!2d13.449374!3d52.504118!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e508c927b8f%3A0x706f72ab069c5642!2sRotherstra%C3%9Fe+16%2C+10245+Berlin!5e0!3m2!1sde!2sde!4v1410822929009" width="100%" height="367" frameborder="0" style="border:0"></iframe>-->
 </div> 
 
